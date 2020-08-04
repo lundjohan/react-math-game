70cb326ed2dd55ae5f238393d299ca496e023ce4
Johan Lund <drancisdrake@gmail.com>
2020-08-04 23:33:52 +0200

    numbers imported from js-file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
 import Level from './Components/Level'
+import levels from './Database/levels'
 
 function App() {
   const NR_OF_TABLES = 12;
   const ARITMETICS = ["*","+", "/", "-"];
-  let tables = [];
-  for (let i = 0;i <NR_OF_TABLES;i++){
-    tables.push(
-      {
-        leftNr: Math.floor(Math.random()*10),
-        arithmetic: ARITMETICS[Math.floor(Math.random()*ARITMETICS.length)],
-        rightNr: Math.floor(Math.random()*10)
-      }
-    )
-  }
+  let levelOne = levels[0];
+  
+  let tables = levelOne.numbers;
   
   return (
     <div className="App">
---
+++ src/Database/levels.js
@@ -0,0 +1,37 @@
+const levels = [
+    {
+        "level": "1",
+        numbers: [
+            {
+                "leftNr": "4",
+                "rightNr": "5",
+                "arithmetic": "+"
+            },
+            {
+                "leftNr": "9",
+                "rightNr": "3",
+                "arithmetic": "+"
+            }
+
+        ]
+
+    },
+    {
+        "level": "2",
+        numbers: [
+            {
+                "leftNr": "4",
+                "rightNr": "5",
+                "arithmetic": "-"
+            },
+            {
+                "leftNr": "9",
+                "rightNr": "3",
+                "arithmetic": "-"
+            }
+
+        ]
+
+    }
+]
+export default levels;
